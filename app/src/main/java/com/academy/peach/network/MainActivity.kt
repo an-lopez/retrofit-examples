211a6f745801635d60e1dcaa90f3825aa1f7662b
Antonio Lopez <marukkura@gmail.com>
2022-09-22 15:37:00 -0500

    Empty state

--- app/src/main/java/com/academy/peach/network/MainActivity.kt
+++ app/src/main/java/com/academy/peach/network/MainActivity.kt
@@ -3,7 +3,6 @@ package com.academy.peach.network
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.viewModels
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -12,38 +11,20 @@ import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.collectAsState
-import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import coil.compose.AsyncImage
-import com.academy.peach.network.network.service.RickAndMortyClient
 import com.academy.peach.network.model.network.response.Character
-import com.academy.peach.network.network.RetrofitClient
-import com.academy.peach.network.network.service.RickAndMortyService
-import com.academy.peach.network.ui.characters.CharacterUiState
-import com.academy.peach.network.ui.characters.FeaturedCharactersViewModel
-import com.academy.peach.network.ui.characters.FeaturedCharactersViewModelFactory
 import com.academy.peach.network.ui.theme.RetrofitCallTheme
 
 class MainActivity : ComponentActivity() {
 
-    private val viewModel: FeaturedCharactersViewModel by viewModels {
-        FeaturedCharactersViewModelFactory(
-            RickAndMortyClient(
-                RetrofitClient.retrofitClient.create(
-                    RickAndMortyService::class.java
-                )
-            )
-        )
-    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
 
-            val state: CharacterUiState by viewModel.characters.collectAsState()
 
             RetrofitCallTheme {
                 // A surface container using the 'background' color from the theme
@@ -53,33 +34,7 @@ class MainActivity : ComponentActivity() {
                 ) {
 
                     Column(Modifier.fillMaxSize()) {
-                        Row(
-                            modifier = Modifier.fillMaxWidth(),
-                            horizontalArrangement = Arrangement.spacedBy(
-                                4.dp,
-                                Alignment.CenterHorizontally
-                            )
-                        ) {
-                            Button(onClick = viewModel::getCharacters) {
-                                Text(text = "Characters")
-                            }
-                            Button(onClick = viewModel::getError) {
-                                Text(text = "Error")
-                            }
-                            Button(onClick = viewModel::getException) {
-                                Text(text = "Exception")
-                            }
-                        }
-
-                        CharactersList(state.characters)
-
-                        state.errorMessage?.let {
-                            Text(text = it)
-                        }
-
-                        state.exception?.let {
-                            Text(text = it.message ?: "Empty exception")
-                        }
+                        CharactersList(emptyList())
                     }
                 }
             }
